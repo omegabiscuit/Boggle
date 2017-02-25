057922cbd915c84e3d18461b1058c22ce1176dff
Brigham Bangerter <bbangerter829@gmail.com>
2017-02-25 11:49:49 -0500

    added boolean method to prevent repeating same cell

--- src/Node.java
+++ src/Node.java
@@ -7,30 +7,44 @@ import java.util.ArrayList;
 public class Node {
 
 
-    public Node(int value) {
+    public Node(int value) { //Node constructor
         this.value = value;
     }
 
-    public int getValue() {
+    public int getValue() { //return node value
         return value;
     }
 
-    public void addChild(Node node) {
+    public void addChild(Node node) { //add onto solution path
+        if(parent == node){ //prevents backtracking
+            return;
+        }
         children.add(node);
         node.setParent(this);
 
     }
 
-    public Node getParent() {
+    public boolean isAncestorOf(Node node){ //checks if node is already in solution path.
+        Node tmp = this;
+        while(tmp.getValue() != 0){
+            if(tmp.parent == node){
+                return true;
+            }
+            tmp = tmp.getParent();
+        }
+        return false;
+    }
+
+    public Node getParent() { //get previous value in solution path
         return parent;
     }
 
-    protected void setParent(Node node) {
+    protected void setParent(Node node) { //set previous value in solution path
         parent = node;
     }
 
     private int value = 0;
-    private ArrayList<Node> children = new ArrayList<>();
+    private ArrayList<Node> children = new ArrayList<>(); //list of possible next values in solution path
     private Node parent = null;
 
 }
